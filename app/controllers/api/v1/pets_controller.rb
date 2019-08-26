02beeb1a95313b25fca482760dc0966d0ffef6fc
Annie Phan <anniephan.atp@gmail.com>
2019-08-26 11:10:55 -0700

    add shelters controller

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::PetsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def pet_params
+
+  end
+
 end
--- app/controllers/api/v1/shelters_controller.rb
+++ app/controllers/api/v1/shelters_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::SheltersController < ApplicationController
+
+  def index
+    @shelters = Shelter.all
+    render json: @shelters
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+    render json: @shelter
+  end
+
+  def create
+    @shelter = Shelter.new(shelter_params)
+    if @shelter.save
+      render json: @shelter
+    else
+      render json: {error: 'Error creating shelter'}
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    @shelter = Shelter.find(params[:id])
+    @shelter.destroy
+  end
+
+  private
+
+  def shelter_params
+    params.require(:shelter).permit(:name, :street_address, :city, :state, :zipcode, :email, :number)
+  end
 end
